5d8692faf2d5d545b7ebd93c845df4ed69aaa68b
Jean-Louis HERVE <81361532+jeanlouisHERVE@users.noreply.github.com>
2022-12-05 13:45:59 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,6 +1,15 @@
 node default {
+  file { '/root/README':
+    ensure  => 'file',
+    content => 'This is a readme',
+    owner   => 'root',
+  }
 }
 
 node 'master.puppet.vm' {
   include role::master_server
+}
+
+node /^db/ {
+  include role::db_server
 }
