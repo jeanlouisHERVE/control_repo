f69bd5a7dfaffcde96e2dbb695e1115ee8db2eba
Jean-Louis HERVE <81361532+jeanlouisHERVE@users.noreply.github.com>
2022-12-06 14:34:00 +0100

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,8 +8,16 @@ node default {
 
 node 'master.puppet.vm' {
   include role::master_server
+  file '/root/README':
+    ensure  => 'file',
+    content => $fqdn,
+  }
 }
 
 node /^db/ {
   include role::db_server
+}
+
+node /^web/ {
+  include role::app_server
 }
